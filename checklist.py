59ab182a4923007be9b4aca20b00cb4d06579465
Hernan415 <hernan.hernandezmendoza@students.dominican.edu>
2021-09-14 10:03:15 -0700

    update checklist.py

--- checklist.py
+++ checklist.py
@@ -23,32 +23,24 @@ def list_all_items():
         print("{} {}".format(index, list_item))
         index += 1
 
-def mark_completed(index):
-    index = 0
-    for list_item in Checklist:
-        checklist.append('√' + Checklist)
-
-def user_input(prompt):
-    # the input function will display a message in the terminal
-    # and wait for user input.
-    user_input = input(prompt)
-    return user_input
-user_value = user_input("Please Enter a value:")
-print(user_value)
-
-
+#def mark_completed(index):
+#    index = 0
+#    for list_item in Checklist:
+#        checklist.append('√' + Checklist)
 def select(function_code):
 
-    if function_code == "C":
+    if function_code.upper() == "C":
         #Create item in checklist here
-
-    elif function_code == "R":
+        input_item = user_input("Input item:")
+        create(item)
+    elif function_code.upper() == "R":
+        item_index = user_input("Index Number?")
         # Read item in checklist here
-
-    elif function_code == "P":
+        read(item_index)
+    elif function_code.upper() == "P":
         # Print all items here
-
-    elif function_code == "Q":
+        list_all_items()
+    elif function_code.upper() == "Q":
         # This is where we want to stop our loop
         return False
     else:
@@ -56,35 +48,43 @@ def select(function_code):
         print("Unknown Option")
     return True
 
-def test():
-    create("purple sox")
-    create("red cloak")
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input
 
-    print(read(0))
-    print(read(1))
-
-    update(0, "purple socks")
-
-    destroy(1)
-
-    print(read(0))
-
-    mark_completed(1)
-
-    list_all_items()
-     # Call your new function with the appropriate value
-    select("C")
-    # View the results
-    list_all_items()
-    # Call function with new value
-    select("R")
-    # View results
-    list_all_items()
-    # Continue until all code is run
-
-test()
+item = user_input("Please Enter an Item: ")
+print(item)
+#def test():
+#    create("purple sox")
+#    create("red cloak")
+#
+#    print(read(0))
+#    print(read(1))
+#
+#    update(0, "purple socks")
+#
+#    destroy(1)
+#
+#    print(read(0))
+#
+#    mark_completed(1)
+#
+#    list_all_items()
+#     # Call your new function with the appropriate value
+#    select("C")
+#    # View the results
+#    list_all_items()
+#    # Call function with new value
+#    select("R")
+#    # View results
+#    list_all_items()
+#    # Continue until all code is run
+#
+#test()
 running = True
 while running:
     selection = user_input(
-        "Press C to add to list, R to Read from list and P to display list")
-    select(selection)
+        "Press C to add to list, R to Read from list, P to display list, and Q to quit: ")
+    running = select(selection)
